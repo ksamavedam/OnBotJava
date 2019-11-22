fc8c3e111e92f6b3e46c2ac8dbcae0420295be4e
ksamavedam <43559712+ksamavedam@users.noreply.github.com>
2019-11-22 07:53:14 -0500

    Got basic TFlow working

--- org/firstinspires/ftc/teamcode/OtterMelonAutonomous.java
+++ org/firstinspires/ftc/teamcode/OtterMelonAutonomous.java
@@ -22,6 +22,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 import java.util.List;
+
 import java.lang.*;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
@@ -29,43 +30,52 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @Autonomous(name = "SkyStoneAuto", group = "Linear Opmode")
 public class OtterMelonAutonomous extends LinearOpMode {
     RobotHardware hw = null;
     private RobotDrive rd = null;
     private RobotSense rs = null;
-    
+    private static final String robotName = "VuforiaTest";
+    // private static final String robotName = "OtterMelon";
+    int ssDetCount = 0;
+    int ssNotDetCount = 0;
+
     @Override
     public void runOpMode() {
+        // These must be initialized in the runOpmode
+        hw = new RobotHardware(robotName, hardwareMap);
+        // rd = new RobotDrive(hw);
+        rs = new RobotSense(hw, telemetry);
 
-        hw = new RobotHardware("OtterMelon", hardwareMap);
-        rd = new RobotDrive(hw);
-        rs = new RobotSense();
+        telemetry.addData("Ready! ", "Go Flamangos!"); 
+        telemetry.update();
+
         waitForStart();
-        while(opModeIsActive()){
-
-            goSquare();
+        while (opModeIsActive()) {
+            playVuforia();
         }
 
-
-
-
-        // Vuforia, scan a picture and decide wheter you are on Loading side or Building
-        // side
-        
-       // RobotSense.Zone whichSide = rs.scanBegin();
-
-  
-        // Loading zone: Robot starts in the loading zone
-        /*if (whichSide == RobotSense.Zone.LOADING) {
-            LoadingZone();
-
-        }*/
+        rs.shutdown();
     }
 
+    public void playVuforia() {
 
-    public void goSquare(){
+        double[] d = rs.locateSkystone(100);
+        // distance and angle are valid only if the skystone is detected.
+        if (d[0] == 1) {
+            ssDetCount++;
+            telemetry.addData("ITS A SKYSTONE, DetCount:", "%d ssNotDetCount:%d", ssDetCount, ssNotDetCount);
+            telemetry.addData("Skystone Dist - Angle ", "%f - %f", d[1], d[2]);
+
+        } else {
+            ssNotDetCount++;
+        }
+        telemetry.update();
+    }
+
+    public void goSquare() {
 
         rd.moveDist(RobotDrive.Direction.FORWARD, 15, .5);
         rd.moveDist(RobotDrive.Direction.LEFT, 15, .5);
@@ -75,48 +85,48 @@ public class OtterMelonAutonomous extends LinearOpMode {
 
     public void LoadingZone() {
         /*
-        double speed_for_sstone = 0.3;  // why this speed ? 
-        double speed_for_blad = 0.9; 
-
-        
-         //Start by scanning each stone to see if it is a skystone
-        
-          //if robot sees a skystone return true while(!rs.isSkystone()){ //while the
-          // robot does not see a skyStone keep moving
-         rd.move(RobotDrive.Direction.RIGHT, 8.0, speed_for_sstone); }
-          
-          // robot now sees a skyskystone. // robotSense should give back the distance
-          //to which the robot needs to travel // in an array [horizontal
-          // distance,forward distance]
-         
-          double[] distance = rs.locateSkystone();
-          rd.move(RobotDrive.Direction.RIGHT,distance[0], speed_for_sstone);
-          rd.move(RobotDrive.Direction.FORWARD,distance[1], speed_for_blad);
-          
-          //drop the arm to drag the skystone
-          
-          //go backward with the skystone rd.move(RobotDrive.Direction.REVERSE,double
-          // distance,double speed); 
-          rd.move(RobotDrive.Direction.LEFT, distance, speed); // direction needed to travel to deliver the stone
-          
-          rd.move(RobotDrive.Direction.RIGHT, distance, speed); // direction needed to travel to deliver the stone
-          
-          
-          //move to the second skystone. //you are now infront of where the first
-          //skystone used to be,. you now where the second skystone is
-          
-          //the second skystone is 3 blocks over
-          rd.move(RobotDrive.Direction.Right,24, speed);
-          
-          //drop the arm to drag the skystone
-          
-          //go backward with the skystone 
-          rd.move(RobotDrive.Direction.REVERSE,  distance, speed); 
-          rd.move(RobotDrive.Direction.LEFT, distance, speed); //direction needed to travel to deliver the stone
-          
-          
-          //park 
-          rd.move(RobotDrive.Direction.RIGHT, distance, speed);
+         * double speed_for_sstone = 0.3; // why this speed ? double speed_for_blad =
+         * 0.9;
+         * 
+         * 
+         * //Start by scanning each stone to see if it is a skystone
+         * 
+         * //if robot sees a skystone return true while(!rs.isSkystone()){ //while the
+         * // robot does not see a skyStone keep moving
+         * rd.move(RobotDrive.Direction.RIGHT, 8.0, speed_for_sstone); }
+         * 
+         * // robot now sees a skyskystone. // robotSense should give back the distance
+         * //to which the robot needs to travel // in an array [horizontal //
+         * distance,forward distance]
+         * 
+         * double[] distance = rs.locateSkystone();
+         * rd.move(RobotDrive.Direction.RIGHT,distance[0], speed_for_sstone);
+         * rd.move(RobotDrive.Direction.FORWARD,distance[1], speed_for_blad);
+         * 
+         * //drop the arm to drag the skystone
+         * 
+         * //go backward with the skystone rd.move(RobotDrive.Direction.REVERSE,double
+         * // distance,double speed); rd.move(RobotDrive.Direction.LEFT, distance,
+         * speed); // direction needed to travel to deliver the stone
+         * 
+         * rd.move(RobotDrive.Direction.RIGHT, distance, speed); // direction needed to
+         * travel to deliver the stone
+         * 
+         * 
+         * //move to the second skystone. //you are now infront of where the first
+         * //skystone used to be,. you now where the second skystone is
+         * 
+         * //the second skystone is 3 blocks over rd.move(RobotDrive.Direction.Right,24,
+         * speed);
+         * 
+         * //drop the arm to drag the skystone
+         * 
+         * //go backward with the skystone rd.move(RobotDrive.Direction.REVERSE,
+         * distance, speed); rd.move(RobotDrive.Direction.LEFT, distance, speed);
+         * //direction needed to travel to deliver the stone
+         * 
+         * 
+         * //park rd.move(RobotDrive.Direction.RIGHT, distance, speed);
          */
     }
 
