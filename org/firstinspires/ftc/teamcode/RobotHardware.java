98f5d7ea1de16d2b4be8515b2b2fd907bd47a4b4
ksamavedam <43559712+ksamavedam@users.noreply.github.com>
2019-11-22 07:53:14 -0500

    Got basic TFlow working

--- org/firstinspires/ftc/teamcode/RobotHardware.java
+++ org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -4,7 +4,14 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.hardware.bosch.BNO055IMU;
-
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 
 public class RobotHardware {
     public BNO055IMU imu;
@@ -14,19 +21,29 @@ public class RobotHardware {
     public DcMotor trMotor=null;
     public DcMotor intakeMotorLeft=null;
     public DcMotor intakeMotorRight=null;
+    public DistanceSensor sensorRange=null;
     double ticksToInchV = 32.0;
     double ticksToInchH = 37.0;
     double ticksToInchR = 15.0;
     double ticksToInchD = 49.0;
     HardwareMap hwMap = null;
+    public TFObjectDetector tfod;
+    public VuforiaLocalizer vuforia;
+    private static final String VUFORIA_KEY = 
+    "AQapiaj/////AAABmR9MFwGXtUXlokDNoVqBfPgVJQUtQjEGM5ThSHmsuy4picaSUk8W+xn1vM+EV1DbJfrr58EOVEJdMfLFvG4An8oN8YDvHB44IGFPAmQBdHv3RkhbYEgWU/guwcEjwIXtcRTRt/J0PmTZG2xnyDxFfAk+AOUVtLE/Ze481z/We0oTolHGpStuPrUhGKGQcY7noVFb2q2LU/3DRoUKg7R1P7y93lluKthHr2BXZSFuHqN4CmEzXdeJKQ+vZrJxorguqiIdyiNvJ1fjXAtCATQeLAOwGQWp7HlRb/T9UUf/KXLcYxKKQV3NPtvE3iPuRkDceF3IvbPbX3i32+MKXZqKFHhpZwomR9PpqEzaxgG8pJ1I";
+    private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
+    private static final String LABEL_FIRST_ELEMENT = "Stone";
+    private static final String LABEL_SECOND_ELEMENT = "Skystone";  
 
     public RobotHardware(String name, HardwareMap hw) {
+
+
         if (name == "OtterMelon") {
             ticksToInchV = 32.0;
             ticksToInchH = 37.0;
             ticksToInchR = 15.0;
             ticksToInchD = 49.0;
-        } else if (name == "{Ri3D}") {
+        } else if (name == "Ri3D") {
             // TBD
             ticksToInchV = 32.0;
             ticksToInchH = 37.0;
@@ -35,30 +52,43 @@ public class RobotHardware {
         }
 
         hwMap = hw;
+        if (name == "OtterMelon") {
+            tlMotor = hwMap.get(DcMotor.class, "topLeft");
+            blMotor = hwMap.get(DcMotor.class, "bottomLeft");
+            brMotor = hwMap.get(DcMotor.class, "bottomRight");
+            trMotor = hwMap.get(DcMotor.class, "topRight");
+            sensorRange = hwMap.get(DistanceSensor.class, "distanceS");
 
-        tlMotor = hwMap.get(DcMotor.class, "topLeft");
-
-        blMotor = hwMap.get(DcMotor.class, "bottomLeft");
-        brMotor = hwMap.get(DcMotor.class, "bottomRight");
-        trMotor = hwMap.get(DcMotor.class, "topRight");
-        
-
-        imu = hwMap.get(BNO055IMU.class, "imu");
-        BNO055IMU.Parameters imuParameters = new BNO055IMU.Parameters();
-        // Use degrees as angle unit.
-        imuParameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
-        // Express acceleration as m/s^2.
-        imuParameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        // Disable logging.
-        imuParameters.loggingEnabled = false;
-        // Initialize IMU.
-        imu.initialize(imuParameters);
-
+            imu = hwMap.get(BNO055IMU.class, "imu");
+            BNO055IMU.Parameters imuParameters = new BNO055IMU.Parameters();
+            // Use degrees as angle unit.
+            imuParameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+            // Express acceleration as m/s^2.
+            imuParameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+            // Disable logging.
+            imuParameters.loggingEnabled = false;
+            // Initialize IMU.
+            imu.initialize(imuParameters);
+            // telemetry.addData("%s", "  ITS A SKYSTONE");
+        }
     }
     public double getTicksToInchV() {return ticksToInchV; }
     public double getTicksToInchH() {return ticksToInchH; }
     public double getTicksToInchR() {return ticksToInchR; }
     public double getTicksToInchD() {return ticksToInchD; }
+    public void initTfod() {
+        int tfodMonitorViewId = hwMap.appContext.getResources().getIdentifier("tfodMonitorViewId", "id",
+        hwMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minimumConfidence = 0.8;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
+    }
 
-    
+    public void initVuforia() {
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraDirection = CameraDirection.BACK;
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+    }
 }
--- org/firstinspires/ftc/teamcode/RobotSense.java
+++ org/firstinspires/ftc/teamcode/RobotSense.java
@@ -9,48 +9,154 @@ import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.teamcode.RobotHardware;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import java.lang.Math;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class RobotSense {
-/*    
-    RobotHardware hw;
 
-    enum Zone { LOADING, BUILDING};
+    RobotHardware hw;
+    Telemetry telemetry;
+    private TFObjectDetector tfod;
+    private VuforiaLocalizer vuforia;
+    double dist;
+
+    enum Zone {
+        LOADING, BUILDING
+    };
+
+    enum Direction {
+        LEFT, RIGHT, NONE
+    };
+
+    public class DirectionVals {
+        RobotSense.Direction dir;
+        double distance;
+        double angle;
+
+        DirectionVals() {
+            dir = Direction.NONE;
+            distance = 0;
+            angle = 0;
+        }
+    };
+
     // initialize later
-RobotSense(RobotHardware rhw) {
-    hw = rhw; 
-    initTfod();
-    initVuforia();
-}
+    RobotSense(RobotHardware rhw, Telemetry t) {
+        hw = rhw;
+        telemetry = t;
+        dist = 0;
+        hw.initVuforia();
+
+        if (ClassFactory.getInstance().canCreateTFObjectDetector()) {
+            hw.initTfod();
+        } else {
+            telemetry.addData("Sorry!", "This device is not compatible with TFOD");
+        }
+        tfod = hw.tfod;
+        vuforia = hw.vuforia;
+
+        if (tfod != null) {
+            tfod.activate();
+        }
+
+    }
+
+    void shutdown() {
+        if (tfod != null) {
+            tfod.shutdown();
+        }
+    }
 
     // give what zone the robot is in (use the getCoordinates )
     public void getZone() {
 
     }
 
-    // boolean for whether skystone is in sight; t = sky, f = no
-    public boolean isSkystone() {
+    // Some how this is not working; do not use this
+    // use locateSkystone instead
+    public boolean isSkystoneInSight() {
+        return false;
+    }
+
+    // Test function - not used anywhere
+    public void detectSkystone() {
+        if (tfod != null) {
+            // getUpdatedRecognitions() will return null if no new information is available
+            // since
+            // the last time that call was made.
+            List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+            if (updatedRecognitions != null) {
+                telemetry.addData("# Object Detected", updatedRecognitions.size());
+
+                // step through the list of recognitions and display boundary info.
+                int i = 0;
+                for (Recognition recognition : updatedRecognitions) {
+                    telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                    telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f", recognition.getLeft(),
+                            recognition.getTop());
+                    telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f", recognition.getRight(),
+                            recognition.getBottom());
+                }
+                telemetry.update();
+            }
+        }
 
     }
 
     // detect skystone and determine relative position in inches
-    public double[] locateSkystone() {
+    // Pass iteration count which is number of detection attempts should be done
+    // Return array
+    // index 0: 1-detected or 0-not
+    // index 1 - Distance
+    // index 2 - Angle
+    public double[] locateSkystone(int iterations) {
+        double[] dist_angle = new double[3];
+        double detected = 0;
+        int count = 0; 
+        while (count++ < iterations) {
+            if (tfod != null) {
+                List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                if (updatedRecognitions != null) {
 
+                    for (Recognition recognition : updatedRecognitions) {
+                        double d_angle = recognition.estimateAngleToObject(AngleUnit.DEGREES);
+                        if (recognition.getLabel() == "Skystone") {
+                            dist_angle[2] = recognition.estimateAngleToObject(AngleUnit.DEGREES);
+                            detected = 1;
+                            break; // as soon as we detect, break and return the results
+                        }
+                    }
+                }
+            }
+        }
+        dist_angle[0] = detected;
+        dist_angle[1] = calcDiagMove(dist_angle[2], getDistance());
+
+        return dist_angle;
+    }
+
+    private double getDistance() {
+        return dist++;
+        // return hw.sensorRange.getDistance(DistanceUnit.INCH);
     }
 
     // returns angle using imu
     public double imuAngle() {
 
+        return 0;
     }
 
     // returns coordinates to getZone to determine what side we're on
     private double getCoordinates() {
 
+        return 0;
     }
 
     public double calcHorizMove(double d_angle, double distance) {
@@ -64,21 +170,19 @@ RobotSense(RobotHardware rhw) {
         double diag_move = (distance / Math.cos(r_angle));
         return diag_move;
     }
-
-    private void initTfod() {
-        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("tfodMonitorViewId", "id",
-                hardwareMap.appContext.getPackageName());
-        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-        tfodParameters.minimumConfidence = 0.8;
-        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
-        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
-    }
-
-    private void initVuforia() {
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
-        parameters.vuforiaLicenseKey = VUFORIA_KEY;
-        parameters.cameraDirection = CameraDirection.BACK;
-        vuforia = ClassFactory.getInstance().createVuforia(parameters);
-    }
-*/
+    /*
+     * private void initTfod() { int tfodMonitorViewId =
+     * hw.appContext.getResources().getIdentifier("tfodMonitorViewId", "id",
+     * hw.appContext.getPackageName()); TFObjectDetector.Parameters tfodParameters =
+     * new TFObjectDetector.Parameters(tfodMonitorViewId);
+     * tfodParameters.minimumConfidence = 0.8; tfod =
+     * ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+     * tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT,
+     * LABEL_SECOND_ELEMENT); }
+     * 
+     * private void initVuforia() { VuforiaLocalizer.Parameters parameters = new
+     * VuforiaLocalizer.Parameters(); parameters.vuforiaLicenseKey = VUFORIA_KEY;
+     * parameters.cameraDirection = CameraDirection.BACK; vuforia =
+     * ClassFactory.getInstance().createVuforia(parameters); }
+     */
 }
