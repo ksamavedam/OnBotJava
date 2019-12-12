14aa3d6a3b97dfaf6a83dbf15fc95bfed46de5bf
maanav13579 <maanav13579@gmail.com>
2019-12-11 19:56:15 -0500

    latest

--- org/firstinspires/ftc/teamcode/RobotHardware.java
+++ org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -23,6 +23,10 @@ public class RobotHardware {
     public DcMotor intakeMotorLeft=null;
     public DcMotor intakeMotorRight=null;
     public DistanceSensor sensorRange=null;
+    public Servo gripper;
+    public Servo armRight;
+    public Servo armLeft;
+    public Servo level;
     double ticksToInchV = 32.0;
     double ticksToInchH = 37.0;
     double ticksToInchR = 15.0;
@@ -57,6 +61,11 @@ public class RobotHardware {
         blMotor = hwMap.get(DcMotor.class, "bottomLeft");
         brMotor = hwMap.get(DcMotor.class, "bottomRight");
         trMotor = hwMap.get(DcMotor.class, "topRight");
+        gripper=hwMap.get(Servo.class, "gripper");
+        armLeft=hwMap.get(Servo.class, "armLeft");
+        armRight=hwMap.get(Servo.class, "armRight");
+        level=hwMap.get(Servo.class, "level");
+        
         
 
         imu = hwMap.get(BNO055IMU.class, "imu");
---
+++ org/firstinspires/ftc/teamcode/TestingTele.java
@@ -0,0 +1,64 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Blinker;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import java.util.Arrays;
+import java.lang.Math;
+
+@TeleOp(name = "TestingTele", group = "TeleOpModes")
+
+public class TestingTele extends LinearOpMode {
+    private BNO055IMU imu;
+
+    public ElapsedTime mRunTime = new ElapsedTime();
+
+    public Servo gripper;
+    public Servo armRight;
+    public Servo armLeft;
+    public Servo level;
+    @Override
+    public void runOpMode() {
+        gripper=hardwareMap.get(Servo.class, "gripper");
+        armLeft=hardwareMap.get(Servo.class, "armLeft");
+        armRight=hardwareMap.get(Servo.class, "armRight");
+        level=hardwareMap.get(Servo.class, "level");
+    
+        waitForStart();
+        double s1Pos=.3;
+        while (opModeIsActive()) {
+           
+            if (gamepad1.a) {
+
+                
+                s1Pos=.4;
+            } else if (gamepad1.b) {
+
+                s1Pos=.2;
+            } 
+            else if (gamepad1.x) {
+
+                s1Pos=.3;
+            } else if (gamepad1.y) {
+
+                s1Pos=.5;
+            }
+            
+            
+            armRight.setPosition(1-s1Pos);
+            armLeft.setPosition(s1Pos);
+            level.setPosition(s1Pos+.08);
+        }
+    }
+}
