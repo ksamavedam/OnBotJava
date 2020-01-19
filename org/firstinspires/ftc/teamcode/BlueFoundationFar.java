221d0c47ed2a7d9ca6a9cd3adc346fb69d2ff643
Ryan Kim <44306909+BosssBurger-Flamangos@users.noreply.github.com>
2020-01-19 15:07:05 -0500

    "Foundation Far" comments (Ryan)

--- org/firstinspires/ftc/teamcode/BlueFoundationFar.java
+++ org/firstinspires/ftc/teamcode/BlueFoundationFar.java
@@ -62,6 +62,7 @@ public class BlueFoundationFar extends LinearOpMode {
         telemetry.addData("Ready! ", "Go Flamangos!"); 
         telemetry.update();
 
+        //Start the servos in the correct starting positions
         hw.armRight.setPosition(1-.03);
         hw.armLeft.setPosition(.03);
         hw.level.setPosition(.03+.05);
@@ -71,6 +72,9 @@ public class BlueFoundationFar extends LinearOpMode {
         waitForStart();
         while (opModeIsActive()) {
 
+            /*Start in the building zone
+              Go for the foundation
+              Park*/
             BuildingZone();
             break;
         }
@@ -85,29 +89,30 @@ public class BlueFoundationFar extends LinearOpMode {
         //set gripper so it doesn't run into bridge
         hw.gripper.setPosition(.3);
 
-        //lock foundation
+        //Approach the foundation
         rd.moveDist(RobotDrive.Direction.REVERSE, 20, .5);
         rd.moveDist(RobotDrive.Direction.REVERSE, 10.5, .2);
 
+        //Latch onto the foundation
         hw.f_servoRight.setPosition(1);
         hw.f_servoLeft.setPosition(.5);
         sleep(1500);
 
-        
-        //move and turn to be parallel to bridge
-        
+        //Align the robot to be parallel to the bridge
         rd.moveDist(RobotDrive.Direction.LEFT,17, .3);
         rd.proportionalTurn(90,1.5);
         rd.resetEncoders();
 
-        //score foundation and unlock
+        //Move the foundation into the building zone
         rd.moveDist(RobotDrive.Direction.RIGHT, 15, .5);
         rd.moveDist(RobotDrive.Direction.REVERSE, 18, .5);
+
+        //Unlatch from the foundation
         hw.f_servoRight.setPosition(.5);
         hw.f_servoLeft.setPosition(1);
         sleep(1500);
 
-        //move to wall and park
+        //Move under the bridge and park
         rd.moveDist(RobotDrive.Direction.RIGHT, 7, .5);
         rd.moveDist(RobotDrive.Direction.FORWARD, 37, .5);
         hw.f_servoLeft.setPosition(1);
